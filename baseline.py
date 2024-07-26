6e0524702f41b0fa04d6b805a52f8a4ab457d9b2
Mengjie Xu <57291560+oxon-612@users.noreply.github.com>
2024-07-26 08:52:56 +0800

    Update baseline.py

--- baseline.py
+++ baseline.py
@@ -9,21 +9,26 @@ import random
 from util import *
 
 class BaselineModel(torch.nn.Module):
-    def __init__(self, num_features=5, hidden_size=32, target_size=1):
+    def __init__(self, num_features=3, hidden_size=32, target_size=1,num_layers=2):
         super().__init__()
         self.hidden_size = hidden_size
         self.num_features = num_features
         self.target_size = target_size
-        self.convs = [GATConv(self.num_features, self.hidden_size),
-                      GATConv(self.hidden_size, self.hidden_size)]
+        self.num_layers = num_layers
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size, num_layers=self.num_layers, batch_first=True)
+        self.convs = GATConv(self.num_features, self.hidden_size)
         self.linear = nn.Linear(self.hidden_size, self.target_size)
 
     def forward(self, data):
         x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
-        for conv in self.convs[:-1]: 
-            x = conv(x, edge_index)
-            #x = F.relu(x)
-            x = F.dropout(x, training=self.training)
-        x = self.convs[-1](x, edge_index) 
+        x = F.relu(x)
+        x = self.conv(x, edge_index)
+        # 假设x的维度是(batch_size, num_nodes, hidden_size)，将其调整为GRU输入需要的形状
+        x = x.view(-1, 1, self.hidden_size)  # 调整形状为(batch_size, seq_len, input_size)
+        # 通过GRU层
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)  # 初始化隐藏状态
+        x, _ = self.gru(x, h0)
+        x = x[:, -1, :]  # 取最后一个时间步的输出
+        x = self.conv(x, edge_index) 
         x = self.linear(x)
         return (x) 
